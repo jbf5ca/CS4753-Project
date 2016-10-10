c0535511e0fa360a4dcc3a89ed8af7d16c7abe3a
Ben Fultz <jbf5ca@virginia.edu>
2016-10-09 22:58:35 -0400

    ms 2 stuff

---
+++ about.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+  <title>About Us - Furnish Us</title>
+  <meta name="description" content="free website template" />
+  <meta name="keywords" content="enter your keywords here" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=9" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/image_slide.js"></script>
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="welcome">
+	    <h1><span>Furnish Us</span></h1>
+	  </div><!--end welcome-->
+      <div id="menubar">
+        <ul id="menu">
+          <li><a href="index.php">Home</a></li>
+          <li><a></a></li>
+          <li class="current"><a href="about.php">About Us</a></li>
+          <li><a></a></li>
+          <li><a href="singup.php">Sign Up</a></li>
+        </ul>
+      </div><!--end menubar-->
+    </div><!--end header-->
+    
+	<div id="site_content">  
+	
+      <div id="text_content">
+        <h2>About</h2>
+        <p>Every student at college has trouble packing their car with furniture or selling it to a friend. Look in the dumpster at any college after the school year and you will find piles upon piles of perfectly functional used, and sometimes close to new, furniture. That is why we started Furnish Us.</p>
+        <p>Furnish Us connects college students with other nearby students to buy and sell furniture. We strive not only to gain your money back when selling but also to ease the move in process. College students are stressed enough, so we hope to make this one problem simple and easy: "Furnish your living space and focus on living." Join Furnish Us now and make your furniture needs easier tomorrow!</p>     
+	  </div><!--close text_content-->	
+	
+      <ul class="slideshow">
+        <li class="show"><img width="500" height="450" src="images/home_1.jpg" alt="Furnish Us provides quality used furniture" /></li>
+        <li><img width="500" height="450" src="images/home_2.jpg" alt="Furnish Us provides quality used furniture" /></li>
+      </ul>   
+    
+	</div><!--end site_content--> 
+  </div><!--end main-->  
+
+
+  <div id="footer">
+    <div id="footer_container">
+    <div class="footer_container_box">
+    <p>"Furnish your living space and focus on living"</p>
+    </div><!--close footer_container--> 
+  </div><!--close footer-->  
+  </body>
+</html>
---
+++ singup.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+  <title>Sign Up - Furnish Us</title>
+  <meta name="description" content="free website template" />
+  <meta name="keywords" content="enter your keywords here" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=9" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/image_slide.js"></script>
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="welcome">
+	    <h1><span>Furnish Us</span></h1>
+	  </div><!--end welcome-->
+      <div id="menubar">
+        <ul id="menu">
+          <li><a href="index.php">Home</a></li>
+          <li><a></a></li>
+          <li><a href="about.php">About Us</a></li>
+          <li><a></a></li>
+          <li class="current"><a href="singup.php">Sign Up</a></li>
+        </ul>
+      </div><!--end menubar-->
+    </div><!--end header-->
+    
+	<div id="site_content">  
+	
+      <div id="text_content">
+
+      <form action="register.php" method="post">
+          Name: <input type="text" name="name"><br>
+          Email: <input type="text" name="email"><br>
+          Address: <input type="text" name="address"><br>
+          City: <input type="text" name="city"><br>
+          State: <input type="text" name="state"><br>
+          Zip code: <input type="text" name="zipcode"><br>
+          Password: <input type="password" name="password"><br>
+          Re-enter password: <input type="password" name="password2"><br>
+      <input type="submit" value="Sign Up">
+      </form>
+
+	  </div><!--close text_content-->	
+	
+      <ul class="slideshow">
+        <li class="show"><img width="500" height="450" src="images/home_1.jpg" alt="Furnish Us provides quality used furniture" /></li>
+        <li><img width="500" height="450" src="images/home_2.jpg" alt="Furnish Us provides quality used furniture" /></li>
+      </ul>   
+    
+	</div><!--end site_content--> 
+
+
+  </div><!--end main-->  
+
+
+  <div id="footer">
+    <div id="footer_container">
+    <div class="footer_container_box">
+    <p>"Furnish your living space and focus on living"</p>
+    </div><!--close footer_container--> 
+  </div><!--close footer-->  
+  </body>
+</html>
